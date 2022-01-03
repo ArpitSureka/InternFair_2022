cc1df594f7d9f2c3b3cef69d6d4d36064a4aea1d
Ankit Saraf <ankitntse@gmail.com>
2022-01-03 12:16:38 +0530

    dfsdfsd

--- internfair_iitg/settings.py
+++ internfair_iitg/settings.py
@@ -95,21 +95,21 @@ WSGI_APPLICATION = 'internfair_iitg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'internfair',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'Udgam2022',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'internfair$default',
-        'USER': 'internfair',
-        'PASSWORD': 'if2021@UDGAM',
-        'HOST': 'internfair.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'internfair',
+        'USER': 'postgres',
+        'PASSWORD': 'Udgam2022',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'internfair$default',
+#         'USER': 'internfair',
+#         'PASSWORD': 'if2021@UDGAM',
+#         'HOST': 'internfair.mysql.pythonanywhere-services.com',
+#     }
 }
 
 
